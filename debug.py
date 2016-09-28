e8186b624c201cecbbf241b4d6172feeaeb161fa
john0945 <johnlewis0945@gmail.com>
2016-09-28 22:32:23 +0200

    added debug script

---
+++ debug.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from ryu.cmd import manager
+
+
+def main():
+    sys.argv.append('OpenFlowBackupRules')
+#    sys.argv.append('--verbose')
+    sys.argv.append('--enable-debugger')
+    sys.argv.append('--observe-links')
+
+    manager.main()
+
+if __name__ == '__main__':
+    main()
